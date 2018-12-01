86d82a3ddf9b1a45970fb24bad87756abccbbd72
BagrijRoman <bagrijroman@gmail.com>
2018-12-01 17:25:44 +0200

    add profile logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,91 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 const router = express.Router();
 
-router.get('/test', (req, res) => res.json({ msg: "Profile route works" }));
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { id } = req.user;
+    const errors = {};
+
+    Profile.findOne({ user: id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = 'There is no profile for user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const {
+      user: { id },
+      body: {
+        handle,
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubUserName,
+        skills,
+        youtube,
+        twitter,
+        facebook,
+        linkedin,
+        instagram,
+
+      },
+    } = req;
+    const profileFields = { social: {} };
+    profileFields.user = id;
+    if (handle) { profileFields.handle = handle; }
+    if (company) { profileFields.company = company; }
+    if (website) { profileFields.website = website; }
+    if (location) { profileFields.location = location; }
+    if (bio) { profileFields.bio = bio; }
+    if (status) { profileFields.status = status; }
+    if (githubUserName) { profileFields.githubUserName = githubUserName; }
+    if (typeof skills !== 'undefined') { profileFields.skills = skills.split(','); }
+    if (youtube) { profileFields.social.youtube = youtube; }
+    if (twitter) { profileFields.social.twitter = twitter; }
+    if (facebook) { profileFields.social.facebook = facebook; }
+    if (linkedin) { profileFields.social.linkedin = linkedin; }
+    if (instagram) { profileFields.social.instagram = instagram; }
+
+    Profile.findOne({ user: id })
+      .then(profile => {
+        if (profile) {
+          Profile.findOneAndUpdate(
+            { user: id },
+            { $set: profileFields },
+            { new: true }
+          ).then(profile => res.json(profile));
+        } else {
+          Profile.findOne({ handle: profileFields.handle })
+            .then(profile => {
+              if (profile) {
+                errors.handle = 'That handle already exists';
+                res.status(400).json(errors);
+              }
+              new Profile(profileFields).save().then(profile => res.json(profile));
+            });
+        }
+      });
+  },
+);
+
 
 module.exports = router;
---
+++ validation/profile.js
@@ -0,0 +1,75 @@
+const Validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = validateProfileInput = (data) => {
+  const handle = !isEmpty(data.handle) ? handle.email : '';
+  const status = !isEmpty(data.status) ? data.status : '';
+  const skills = !isEmpty(data.skills) ? data.skills : '';
+  const {
+    website,
+    youtube,
+    twitter,
+    facebook,
+    linkedin,
+    instagram,
+  } = data;
+  let errors = {};
+
+
+  if (!Validator.isLength(handle, { min:2, max: 40 })) {
+    errors.handle = 'Handle needs to be between 2 and 4 characters';
+  }
+
+  if (Validator.isEmpty(handle)) {
+    errors.handle = 'Profile handle is required';
+  }
+
+  if (Validator.isEmpty(status)) {
+    errors.status = 'Status field is required';
+  }
+
+  if (Validator.isEmpty(skills)) {
+    errors.skills = 'Skills field is required';
+  }
+
+  if (!isEmpty(website)) {
+    if (!Validator.isUrl(website)) {
+      errors.website = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(youtube)) {
+    if (!Validator.isUrl(youtube)) {
+      errors.youtube = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(twitter)) {
+    if (!Validator.isUrl(twitter)) {
+      errors.twitter = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(facebook)) {
+    if (!Validator.isUrl(facebook)) {
+      errors.facebook = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(linkedin)) {
+    if (!Validator.isUrl(linkedin)) {
+      errors.linkedin = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(instagram)) {
+    if (!Validator.isUrl(instagram)) {
+      errors.instagram = 'Not a valid URL';
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  }
+};
