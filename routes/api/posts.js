40d5c389c10417c20e8c19872ef5418e915ce00d
BagrijRoman <bagrijroman@gmail.com>
2018-12-02 21:15:34 +0200

    add route to remove like

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -100,4 +100,33 @@ router.post(
   }
 );
 
+router.post(
+  '/unlike/:post_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const {
+      user: { id: userId },
+      params: { post_id: postId }
+    } = req;
+
+    Profile.findOne({ user: userId })
+      .then(profile => {
+        Post.findById(postId)
+          .then(post => {
+            if (!post.likes.filter(({ user }) => user.toString() === userId).length) {
+              return res.status(400).json({ notLiked: 'You have not yet like this post' });
+            }
+
+            const removeIndex = post.likes
+              .map(({ user }) => user.toString())
+              .indexOf(userId);
+            post.likes.splice(removeIndex, 1);
+            post.save().then(post => res.json(post));
+          })
+          .catch(err => res.status(404).json({ postNotFound: 'No post found' }));
+      })
+      .catch(err => res.status(404).json({ profileNotFound: 'No profile found' }));
+  }
+);
+
 module.exports = router;
