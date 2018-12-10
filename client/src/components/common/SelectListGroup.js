c1fde939ebc87720c845f3720fec94c676581dfd
BagrijRoman <bagrijroman@gmail.com>
2018-12-10 21:14:34 +0200

    add select list group component

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const SelectListGroup = ({
+                              name,
+                              value,
+                              error,
+                              info,
+                              onChange,
+                              options,
+                            }) =>
+  <div className="form-group">
+    <select {...{
+      name,
+      onChange,
+      value,
+      className: classnames('form-control form-control-lg', { 'is-invalid': error })
+    }}>
+      {options.map(({ label, value }) =>
+        <option key={label} value={value}>{label}</option>
+      )}
+    </select>
+    {info ? <small className="form-text text-muted">{info}</small> : null}
+    {error ? <div className="invalid-feedback">{error}</div> : null}
+  </div>;
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  options: PropTypes.array.isRequired,
+};
+
+SelectListGroup.defaultProps = {
+  type: 'text',
+};
+
+export default SelectListGroup;
